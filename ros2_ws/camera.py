64845b745922adcbc3dcc66be4ce512e469f214a
Louis <asdfg880226@gmail.com>
2023-04-27 12:36:51 +0000

    20230427

--- ros2_ws/camera.py
+++ ros2_ws/camera.py
@@ -21,20 +21,15 @@ if not cap.isOpened():
 #cv2.namedWindow("live", cv2.WINDOW_AUTOSIZE); # 命名一個視窗，可不寫
 while(True):
     # 擷取影像
+    
     ret, frame = cap.read()
     if not ret:
         print("Can't receive frame (stream end?). Exiting ...")
         break
-
-    # 彩色轉灰階
-    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
     # 顯示圖片
     cv2.imshow('live', frame)
-    #cv2.imshow('live', gray)
 
-    # 按下 q 鍵離開迴圈
-    if cv2.waitKey(1) == ord('Q'):
+    if cv2.waitKey(1) == ord('q'):
         break
 
 # 釋放該攝影機裝置
